a68481f58ec098fe9f2c4d4e739a9937033d7e10
bahar <bahar.navab.79@gmail.com>
2021-11-12 17:56:07 +0330

    Version 1.1.9 Axios has been added. Frontend & backend link has been made. Homepage prototype has been completed.

--- Client/src/components/Home.vue
+++ Client/src/components/Home.vue
@@ -15,67 +15,21 @@
 
 <script>
 import BookGridVue from './BookGrid.vue'
+import api from '../service/api'
 
 export default {
     data() {
         return {
-            books: [
-                {
-                    "id": 1,
-                    "title": "The Hunger Games",
-                    "isbn13": "9780439023480",
-                    "imageUrl": "https://images.gr-assets.com/books/1447303603m/2767052.jpg",
-                    "author": "Suzanne Collins",
-                    "rating": 4.43,
-                    "published_date": "2008-01-01"
-                },
-                {
-                    "id": 2,
-                    "title": "Harry Potter and the Philosopher's Stone",
-                    "isbn13": "9780439554930",
-                    "imageUrl": "https://images.gr-assets.com/books/1474154022m/3.jpg",
-                    "author": "J.K. Rowling, Mary GrandPrÃ",
-                    "rating": 4.44,
-                    "published_date": "1997-01-01"
-                },
-                {
-                    "id": 3,
-                    "title": "Twilight",
-                    "isbn13": "9780316015840",
-                    "imageUrl": "https://images.gr-assets.com/books/1361039443m/41865.jpg",
-                    "author": "Stephenie Meyer",
-                    "rating": 3.57,
-                    "published_date": "2005-01-01"
-                },
-                {
-                    "id": 4,
-                    "title": "To Kill a Mockingbird",
-                    "isbn13": "9780061120080",
-                    "imageUrl": "https://images.gr-assets.com/books/1361975680m/2657.jpg",
-                    "author": "Harper Lee",
-                    "rating": 4.25,
-                    "published_date": "1960-01-01"
-                },
-                {
-                    "id": 3,
-                    "title": "Twilight",
-                    "isbn13": "9780316015840",
-                    "imageUrl": "https://images.gr-assets.com/books/1361039443m/41865.jpg",
-                    "author": "Stephenie Meyer",
-                    "rating": 3.57,
-                    "published_date": "2005-01-01"
-                },
-                {
-                    "id": 4,
-                    "title": "To Kill a Mockingbird",
-                    "isbn13": "9780061120080",
-                    "imageUrl": "https://images.gr-assets.com/books/1361975680m/2657.jpg",
-                    "author": "Harper Lee",
-                    "rating": 4.25,
-                    "published_date": "1960-01-01"
-                }
-            ]
+            books: null,
+            error: null
         };
+    },
+    created() {
+        Promise.resolve(api().get('/books/')).then(res => {
+            this.books = res.data;
+        }).catch(err => {
+            this.error = err;
+        });
     },
     components: {
         appBookGrid: BookGridVue,
