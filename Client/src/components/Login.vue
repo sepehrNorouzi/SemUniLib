b79e25197598675b10ddf49e00e4b03b82056b7a
bahar <bahar.navab.79@gmail.com>
2021-12-29 21:26:45 +0330

    Version 1.1.17 Signup and Login have been modified.

--- Client/src/components/Login.vue
+++ Client/src/components/Login.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="login">
-        <h1>Logiiiiiiiiiiiiiiiiiiiiiiiiiiin</h1>
+        <div class="card">
+            <div class="card-header">Login</div>
+            <div class="card-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" placeholder="John Doe">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password">
+                    </div>
+                    <button class="btn btn-success">Login</button>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
--- Client/src/components/Signup.vue
+++ Client/src/components/Signup.vue
@@ -1,12 +1,35 @@
 <template>
     <div class="signup">
-        <h1>Signuppppppppppppppppppppppppppppppp</h1>
+        <div class="card">
+            <div class="card-header">Signup</div>
+            <div class="card-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" placeholder="John Doe" v-model="userData.username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" v-model="userData.password">
+                    </div>
+                    <button class="btn btn-primary">Signup</button>
+                </form>
+            </div>
+        </div>
     </div>    
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            userData: {
+                username: '',
+                password: '',
+            },
+            err: null,
+        };
+    },
 }
 </script>
 
